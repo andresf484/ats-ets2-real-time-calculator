a2c484dc2ebfeb29f092f97544c91b2d1e3c8bb2
Andrés G <andres88884@hotmail.com>
2022-05-03 20:20:38 -0500

    update

--- js/calculator.js
+++ js/calculator.js
@@ -1,8 +1,8 @@
 // TODO - $(document).ready function
 $(document).ready( function() {
 
-    document.getElementById("txtHours").value = "1";
-    document.getElementById("txtMinutes").value = "00";
+    //document.getElementById("txtHours").value = "1";
+    //document.getElementById("txtMinutes").value = "00";
 
     loadRanges();
 
@@ -31,49 +31,90 @@ function loadRanges(){
 
 }
 
+// TODO - function validateHours
+function validateHours(){
+    let hours = document.getElementById("txtHours").value;
+    if(!isNaN(hours) && hours != '' && hours >= 0){
+        document.getElementById("txtHours").value = hours;
+        return hours;
+    }else if(!isNaN(hours) && hours != '' && hours < 0){
+        document.getElementById("txtHours").value = 0;
+        return '0';
+    }
+}
+
+// TODO - function validateMinutes
+function validateMinutes(){
+    let minutes = document.getElementById("txtMinutes").value;
+    if(!isNaN(minutes) && minutes != '' && (minutes >= 0 && minutes <= 59)){
+        document.getElementById("txtMinutes").value = minutes;
+        return minutes;
+    }else if( !isNaN(minutes) && minutes != '' && minutes < 0 ){
+        document.getElementById("txtMinutes").value = 0;
+        return '0';
+    }else if( !isNaN(minutes) && minutes != '' && minutes > 59 ){
+        document.getElementById("txtMinutes").value = 59;
+        return '59';
+    }
+}
+
 // TODO - function calculate
 function calculate(){
 
     // https://www.tgmtruck.com/2021/07/convertir-tiempo-juego-tiempo-real-en.html
 
-    var hours = document.getElementById("txtHours").value;
-    var minutes = document.getElementById("txtMinutes").value;
+    let hours = validateHours();
+    let minutes = validateMinutes();
 
-    console.log("Datos ingresados: " + hours + ":" + minutes);
+    if ( (!isNaN(hours) && hours != '') && (!isNaN(minutes) && minutes != '') ) {
 
-    var total_minutes_ingame = parseInt(hours) * 60 + parseInt(minutes);
-
-    var game_time_scale = total_minutes_ingame / 19; // Euro truck Simulator 2 time scale "time compression": 1:19
-
-    let load_table = "";
-
-    // 	Tiempo convertido a la realidad
-    if ( game_time_scale >= 60) {
+        var total_minutes_ingame = parseInt(hours) * 60 + parseInt(minutes);
     
-        var converted_hours = game_time_scale / 60;
+        var game_time_scale = total_minutes_ingame / 19; // Euro truck Simulator 2 time scale "time compression": 1:19
     
-        load_table +=`
-
-        <tr>
-            <td class="text-center">`+hours+`:`+minutes+`</td>
-            <td class="text-center">`+``+` `+converted_hours.toFixed(2)+` Horas</td>
-        </tr>
-
-        `;
-
-    } else {
+        let load_table = "";
+    
+        // 	Tiempo convertido a la realidad
+        if ( game_time_scale >= 60) {
         
+            var converted_hours = game_time_scale / 60;
+        
+            load_table +=`
+    
+            <tr>
+                <td class="text-center">`+hours+`:`+minutes+`</td>
+                <td class="text-center">`+``+` `+converted_hours.toFixed(2)+` Horas</td>
+            </tr>
+    
+            `;
+    
+        } else {
+            
+            load_table +=`
+    
+            <tr>
+                <td class="text-center">`+hours+`:`+minutes+`</td>
+                <td class="text-center">`+``+` `+game_time_scale.toFixed(2)+` Minutos</td>
+            </tr>
+    
+            `;
+    
+        }
+    
+        document.getElementById("data_table").innerHTML = load_table;
+
+    }else{
+
+        let load_table = '';
         load_table +=`
-
         <tr>
-            <td class="text-center">`+hours+`:`+minutes+`</td>
-            <td class="text-center">`+``+` `+game_time_scale.toFixed(2)+` Minutos</td>
+            <td class="text-center">00:00 NaN</td>
+            <td class="text-center">00:00 NaN</td>
         </tr>
-
         `;
+
+        document.getElementById("data_table").innerHTML = load_table;
 
     }
-
-    document.getElementById("data_table").innerHTML = load_table;
 
 }
